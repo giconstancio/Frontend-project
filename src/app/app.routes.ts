5d494b7eb9100a7440aa7bf07a99bd17cd48d62a
giconstancio <nunes.constancio950@gmail.com>
2025-06-12 21:36:06 -0300

    feat: Criação de arquivos e configuração de rota.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/home-page',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'home-page',
+    loadComponent: () => import("./home-page/home-page.component").then(c => c.HomePageComponent)
+  },
+
+  {
+    path: 'login-page',
+    loadComponent: () => import("./login-page/login-page.component").then(c => c.LoginPageComponent)
+  }
+
+];
