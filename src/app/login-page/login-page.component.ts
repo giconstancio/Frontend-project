6e3962719607aa2722e48b1f7ad3f81b920c31a0
giconstancio <nunes.constancio950@gmail.com>
2025-06-21 14:23:28 -0300

    feat: Desenvolvimento da home-page e inicio da login-page. Instalação do Angular Material.

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  mostrarSenha = false;
 
+  constructor(
+    private router: Router
+
+  ) {}
+
+  voltarParaHome() {
+    this.router.navigate(['/']);
+  }
+
+
+
+  loginComEmail(email: string, password: string) {
+    if(email && password != null) {
+
+    }
+  }
 }
